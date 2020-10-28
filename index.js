3c1c668e7850ef68dd9b9c9ae4893402d4bfa4f8
ersati <piotrp92@gmail.com>
2020-10-28 21:01:22 +0000

    commit

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ import "./style.css";
 const inputs = document.querySelectorAll("input");
 const prices = document.querySelectorAll(".price");
 const tot = document.querySelector(".total");
-console.log();
+// console.log();
+let t = 0;
+function updateSum(index) {
+  tot.textContent = t;
+  // inputs.forEach((el, i) => {
+  //   let price = prices[i].innerHTML.match(/(\d+)/);
+  //   // console.log(prices[i].innerHTML.match(/(\d+)/));
+  //   let number = price[i];
+  //   // console.log(el.value);
+  //   let total = el.value * number;
 
-function updateSum() {
-  inputs.forEach((el, i) => {
-    let price = prices[i].innerHTML.match(/(\d+)/);
-    console.log(prices[i].innerHTML.match(/(\d+)/));
-    let number = price[i];
-    console.log(el);
-    let total = el * number;
-
-    tot.textContent = total;
-  });
+    
+  // });
 }
 
 inputs.forEach((item, index) => {
+  inputs.forEach((el, i) => {
+    const price = prices[i].innerHTML.match(/(\d+)/)[i];
+    console.log((el.value * price))
+    t += el.value * price;
+  });
   item.addEventListener("change", updateSum);
 });
 
