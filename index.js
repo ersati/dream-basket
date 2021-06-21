c46441f1d388c3257dd305a180c17cac7e9977a2
ersati <piotrp92@gmail.com>
2021-06-21 20:32:25 +0100

    change hoisting of variable total price

--- index.js
+++ index.js
@@ -6,29 +6,36 @@ const prices = document.querySelectorAll('.price');
 const summary = document.querySelector('.total');
 const products = document.querySelectorAll('.product');
 const crosses = document.querySelectorAll('.cross');
+let totalPrice = 0;
 
-console.log(products, crosses);
+console.log(inputs, prices);
 
 function updateSum() {
-  let totalPrice = 0;
+  console.log(inputs.length);
+  totalPrice = 0;
   inputs.forEach((el, i) => {
+    console.log(el);
     const price = prices[i].innerHTML.match(/(\d+)/)[i];
     totalPrice += el.value * price;
   });
+
   return totalPrice;
 }
 
 function showSum() {
   const currencie = 'zł';
-  return (summary.textContent = `${updateSum()} ${currencie}`);
+  summary.textContent = `${updateSum()} ${currencie}`;
+  return;
 }
 
 inputs.forEach(item => {
   item.addEventListener('change', showSum);
 });
+
 crosses.forEach(cross => {
   cross.addEventListener('click', e => {
     console.log('click', e.path[4]);
+
     e.path[4].remove();
   });
 });
