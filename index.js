8f8da1df47a73119f3933e82aed6773316fae286
ersati <piotrp92@gmail.com>
2021-06-21 01:55:29 +0100

    add function do remove elements from the list

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Import stylesheets
-import "./style.css";
+import './style.css';
 
-const inputs = document.querySelectorAll("input");
-const prices = document.querySelectorAll(".price");
-const summary = document.querySelector(".total");
+const inputs = document.querySelectorAll('input');
+const prices = document.querySelectorAll('.price');
+const summary = document.querySelector('.total');
+const products = document.querySelectorAll('.product');
+const crosses = document.querySelectorAll('.cross');
+
+console.log(products, crosses);
 
 function updateSum() {
   let totalPrice = 0;
@@ -15,10 +19,16 @@ function updateSum() {
 }
 
 function showSum() {
-  const currencie = "zł";
+  const currencie = 'zł';
   return (summary.textContent = `${updateSum()} ${currencie}`);
 }
 
 inputs.forEach(item => {
-  item.addEventListener("change", showSum);
+  item.addEventListener('change', showSum);
+});
+crosses.forEach(cross => {
+  cross.addEventListener('click', e => {
+    console.log('click', e.path[4]);
+    e.path[4].remove();
+  });
 });
