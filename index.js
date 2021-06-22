e25e2d1bb56b40077316848e03484d147021cda6
ersati <piotrp92@gmail.com>
2021-06-22 01:57:38 +0100

    fix updatesum to refresh the total amount

--- index.js
+++ index.js
@@ -1,46 +1,62 @@
 // Import stylesheets
 import './style.css';
 
-const inputs = document.querySelectorAll('input');
-const prices = document.querySelectorAll('.price');
 const summary = document.querySelector('.total');
 const products = document.querySelectorAll('.product');
 const crosses = document.querySelectorAll('.cross');
-let totalPrice = 0;
 
-console.log(inputs, prices);
-
-function updateSum() {
-  console.log(inputs.length);
-  totalPrice = 0;
+const updateSum = () => {
+  const inputs = document.querySelectorAll('input');
+  const prices = document.querySelectorAll('.price');
+  let totalPrice = 0;
   inputs.forEach((el, i) => {
-    console.log(el);
-    const price = prices[i].innerHTML.match(/(\d+)/)[i];
-
-    totalPrice += el.value * price;
-    console.log(totalPrice);
+    console.log(typeof el.value, el.value);
+    if (!/[0-9]+/.test(el.value)) {
+      el.value = 1;
+    }
+    const amount = parseInt(el.value);
+    const price = parseInt(prices[i].textContent);
+    totalPrice += amount * price;
   });
 
   return totalPrice;
-}
+};
+
+// const sumPrices = () => {
+//   const prodAmounts = document.querySelectorAll('input');
+//   const prodPrices = document.querySelectorAll('.price');
+//   let totalPrice = 0;
+
+//   prodAmounts.forEach((el, index) => {
+
+//     if (!/[0-9]+/.test(el.value)) {
+//       el.value = 1;
+//     }
+//     const amount = parseInt(el.value);
+//     const price = parseInt(prodPrices[index].textContent);
+
+//     totalPrice += amount * price;
+//   });
+
+//   return totalPrice;
+// };
 
 function showSum() {
   const currencie = 'zł';
   summary.textContent = `${updateSum()} ${currencie}`;
   return;
 }
-
-inputs.forEach(item => {
+function deleteProduct(e) {
+  e.target.closest('.product').remove();
+  console.log(showSum());
+  showSum();
+}
+document.querySelectorAll('input').forEach(item => {
   item.addEventListener('change', showSum);
 });
 
 crosses.forEach(cross => {
-  cross.addEventListener('click', e => {
-    
-
-    e.target.closest('.product').remove();
-    summary.textContent = updateSum();
-  });
+  cross.addEventListener('click', deleteProduct);
 });
 
 showSum();
