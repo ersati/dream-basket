02b44a1dd125bdb6317142c3a7d4a3ffedd7f95b
ersati <piotrp92@gmail.com>
2021-06-23 01:28:30 +0100

    clean the code from console.logs

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const updateSum = () => {
   const prices = document.querySelectorAll('.price');
   let totalPrice = 0;
   inputs.forEach((el, i) => {
-    console.log(typeof el.value, el.value);
     if (!/[0-9]+/.test(el.value)) {
       el.value = 1;
     }
@@ -18,39 +17,23 @@ const updateSum = () => {
     const price = parseInt(prices[i].textContent);
     totalPrice += amount * price;
   });
-
   return totalPrice;
 };
-
-// const sumPrices = () => {
-//   const prodAmounts = document.querySelectorAll('input');
-//   const prodPrices = document.querySelectorAll('.price');
-//   let totalPrice = 0;
-
-//   prodAmounts.forEach((el, index) => {
-
-//     if (!/[0-9]+/.test(el.value)) {
-//       el.value = 1;
-//     }
-//     const amount = parseInt(el.value);
-//     const price = parseInt(prodPrices[index].textContent);
-
-//     totalPrice += amount * price;
-//   });
-
-//   return totalPrice;
-// };
 
 function showSum() {
   const currencie = 'zł';
   summary.textContent = `${updateSum()} ${currencie}`;
+  if (updateSum() === 0) {
+    summary.textContent = 'Nothing in the basket';
+  }
   return;
 }
+
 function deleteProduct(e) {
   e.target.closest('.product').remove();
-  console.log(showSum());
   showSum();
 }
+
 document.querySelectorAll('input').forEach(item => {
   item.addEventListener('change', showSum);
 });
